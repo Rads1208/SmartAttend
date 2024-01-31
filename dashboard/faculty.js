7a26fa7259c068b254159353128995ecf9cc83ee
manasvvi1 <manasvvi.aggarwal@gmail.com>
2024-01-31 13:04:26 +0530

    add new subject and manual attendance added

---
+++ dashboard/faculty.js
@@ -0,0 +1,8 @@
+// import subprocess
+
+// function runPythonScript() {
+//     // Get the path to the Python script.
+//     var pythonScriptPath = "C:/Users/manas/Downloads/SmartAttend/dashboard/face-recognition.py";
+//     // Run the Python script.
+//     subprocess.run(["python", pythonScriptPath]);
+//   }
